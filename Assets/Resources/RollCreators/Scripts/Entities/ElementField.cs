210e9405ba48287863d1decf4313deeb0bac2f20
zhuravlev12 <slawajur@mail.ru>
2020-06-14 17:06:33 +0700

    optimization of resistors

--- Assets/Resources/RollCreators/Scripts/Entities/ElementField.cs
+++ Assets/Resources/RollCreators/Scripts/Entities/ElementField.cs
@@ -43,6 +43,10 @@ public class ElementField : MonoBehaviour
 
     public void UpdateElementSprite(Element e)
     {
+        if (element.type != ElementType.RESISTOR && e.type == ElementType.RESISTOR)
+        {
+            StartCoroutine(Break());
+        }
         element = e;
         spriteRenderer.sprite = Util.GetElementSprite(element);
         staticImage.sprite = Util.GetElementStaticSprite(element);
@@ -69,24 +73,14 @@ public class ElementField : MonoBehaviour
         light.sprite = Util.GetElementLightSprite(element);
         transform.rotation = Quaternion.Euler(0, 0, element.rotation * 90);
         staticImage.gameObject.transform.rotation = Quaternion.Euler(0, 0, -transform.rotation.z);
-        if (element.type == ElementType.RESISTOR)
-        {
-            StartCoroutine(Break());
-        }
     }
 
     private IEnumerator Break()
     {
-        bool connected;
-        do
-        {
-            connected = element.connected;
-            yield return new WaitForSeconds(5); // TODO: timing
-        } while (element.connected != connected);
-
+        yield return new WaitForSeconds(5); // TODO: timing
         if (element.type == ElementType.RESISTOR)
         {
-            if (connected)
+            if (element.connected)
             {
                 element.type = ElementType.BROKEN_RESISTOR;
             }
